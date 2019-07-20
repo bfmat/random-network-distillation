753f539d5473799c97aee17458e6359559077a18
Brendon Matusch <brendon-m@outlook.com>
2019-07-20 19:26:09 -0400

    Use montezuma

--- atari_wrappers.py
+++ atari_wrappers.py
@@ -223,7 +223,7 @@ def make_atari(env_id, max_episode_steps=4500):
 def wrap_deepmind(env, clip_rewards=True, frame_stack=False, scale=False):
     """Configure environment for DeepMind-style Atari.
     """
-    env = CollectGymDataset(env, '~/rnd_outputs')
+    env = CollectGymDataset(env, '~/rnd_outputs_mz')
     env = WarpFrame(env)
     if scale:
         env = ScaledFloatFrame(env)
